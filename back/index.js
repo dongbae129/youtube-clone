5577700a05b2f7f599ebbd717f9a2de26e0bcd81
Dong <dongbae129@naver.com>
2020-04-28 18:10:09 +0900

    finding error

--- back/index.js
+++ back/index.js
@@ -3,11 +3,15 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const db = require("./models");
 const app = express();
+const userAPIRouter = require("./routes/user");
 db.sequelize.sync();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cookieParser());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+app.use("/api/user", userAPIRouter);
 
 app.get("/api/hello", (req, res) => {
   res.send("Hellllllo");
