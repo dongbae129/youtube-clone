d4e80801e4157a1f517a075c3e1446ed41633f34
Dong <dongbae129@naver.com>
2020-05-02 03:39:10 +0900

    end passport login

--- back/index.js
+++ back/index.js
@@ -1,18 +1,46 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const db = require("./models");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const expressSession = require("express-session");
 const userAPIRouter = require("./routes/user");
+const passport = require("passport");
+const passportConfig = require("./passport");
 db.sequelize.sync();
+dotenv.config();
 
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+passportConfig();
 app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: "react-youtube",
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/api/user", userAPIRouter);
 
 app.post("/api/hello", (req, res) => {
--- back/routes/user.js
+++ back/routes/user.js
@@ -1,27 +1,65 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const passport = require("passport");
+// var passport = require("passport"),
+//   LocalStrategy = require("passport-local").Strategy;
+
 const router = express.Router();
 const db = require("../models");
-router.post("/", async (req, res) => {
-  try {
-    const usering = await db.User.findOne({
-      where: { userId: req.body.userId },
-    });
-    if (usering) {
-      return res.status(400).send("이미 사용중인 아이디 입니다");
-    }
-    const hashedPassword = await bcrypt.hash(req.body.password, 12);
-    const user = await db.User.create({
-      nickname: req.body.nickname,
-      userId: req.body.userId,
-      password: hashedPassword,
-    });
 
-    return res.status(200).json(user);
-  } catch (e) {
-    console.error(e);
-  }
+// passport.use(
+//   new LocalStrategy(
+//     {
+//       usernameField: "userId",
+//       passwordField: "password",
+//     },
+//     async (userId, password, done) => {
+//       try {
+//         const user = await db.User.findOne({
+//           where: { userId },
+//         });
+
+//         if (!user) {
+//           return done(null, false, { reason: "존재하지 않는 사용자입니다" });
+//         }
+//         const result = await bcrypt.compare(password, user.password);
+//         if (result) {
+//           return done(null, user);
+//         }
+//         return done(null, false, { reason: "비밀번호가 틀립니다" });
+//       } catch (e) {
+//         return done(e);
+//       }
+//     }
+//   )
+// );
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (info) {
+      return res.status(401).send(info.reason);
+    }
+
+    return req.login(user, async (loginErr) => {
+      try {
+        if (loginErr) {
+          return next(loginErr);
+        }
+        const finalUser = await db.User.findOne({
+          where: { id: user.id },
+        });
+        return res.json(finalUser);
+      } catch (e) {
+        console.error(e);
+        next(e);
+      }
+    });
+  })(req, res, next);
 });
+
 router.post("/signup", async (req, res, next) => {
   try {
     console.log(req.body, "@@@@@@");
@@ -37,18 +75,12 @@ router.post("/signup", async (req, res, next) => {
       nickname: req.body.nickname,
       password: hashedPassword,
     });
-    const sendUser = await db.User.findOne({
-      where: { userId: req.body.userId },
-      attributes: ["nickname", "userId"],
-    });
-    return res.status(200).json(sendUser);
+
+    return res.status(200).json(user);
   } catch (e) {
     console.error(e);
     next(e);
   }
-});
-router.post("/login", (req, res) => {
-  res.send("QQQQQQQQQQQQQQQQQ");
 });
 
 module.exports = router;
